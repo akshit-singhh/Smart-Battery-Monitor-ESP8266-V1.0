8c137ad579e82f269bbcc40984973211d8ad0b6a
Akshit Singh <akshitsingh658@gmail.com>
2025-08-23 02:05:14 +0530

    Update EEPROMUtils.h

--- EEPROMUtils.h
+++ EEPROMUtils.h
@@ -1,3 +1,24 @@
+/*
+   Project   : Smart Battery Monitor (ESP8266 V1.0)
+   File      : EEPROMUtils.h
+   Author    : Akshit Singh (github.com/akshit-singhh)
+   License   : MIT License
+
+   Description:
+   Header file for EEPROMUtils.cpp.
+   Declares EEPROM utility functions for reading and writing floats,
+   integers, and strings.
+
+   Supported APIs:
+   - writeFloat(), readFloat()
+   - writeInt(), readInt()
+   - writeString(), readString()
+
+   Notes:
+   - Works with AT24C32 I2C EEPROM
+   - Integrated with settings persistence across reboots
+*/
+
 #ifndef EEPROM_UTILS_H
 #define EEPROM_UTILS_H
 
